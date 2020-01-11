7f87197ce0c3870df5ab07952f281bad20d14842
Katie Lamber02 <katielamber02@gmail.com>
2020-01-11 17:20:26 +0200

    simple modal

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, Modal, StyleSheet } from 'react-native';
 
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 export default function Home({ navigation }) {
+    const [modalOpen, setModalOpen] = useState(false);
 
     const [reviews, setReviews] = useState([
         { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
@@ -15,6 +17,25 @@ export default function Home({ navigation }) {
 
     return (
         <View style={globalStyles.container}>
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <Ionicons
+                        name='ios-close'
+                        size={24}
+                        style={{ ...styles.modalToggle, ...styles.modalClose }}
+                        onPress={() => setModalOpen(false)}
+                    />
+                    <Text>Hello from Modal</Text>
+                </View>
+            </Modal>
+
+            <Ionicons
+                name='ios-add'
+                size={24}
+                style={styles.modalToggle}
+                onPress={() => setModalOpen(true)}
+            />
+
             <FlatList data={reviews} renderItem={({ item }) => (
                 <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
                     <Card><Text style={globalStyles.titleText}>{item.title}</Text></Card>
@@ -23,3 +44,22 @@ export default function Home({ navigation }) {
         </View>
     );
 }
+const styles = StyleSheet.create({
+    modalToggle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0,
+    },
+    modalContent: {
+        flex: 1,
+    }
+});

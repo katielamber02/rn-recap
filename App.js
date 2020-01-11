c65213f3827ac04c1dcd8ff59d9f24e1bc0c7762
Katie Lamber02 <katielamber02@gmail.com>
2020-01-11 12:43:50 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import Navigator from './routes/homeStack'
+
+const App: () => React$Node = () => {
+  return (
+    <Navigator />
+  );
+};
+
+
+
+export default App;
---
+++ screens/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { globalStyles } from '../styles/global';
+
+export default function Home() {
+    return (
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title="Go to Details" />
+        </View>
+    );
+}

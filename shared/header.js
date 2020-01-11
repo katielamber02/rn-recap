116d3d2066bc81189876e1b22464da76c326e5e2
Katie Lamber02 <katielamber02@gmail.com>
2020-01-11 17:06:25 +0200

    background image in header

--- shared/header.js
+++ shared/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
@@ -10,33 +10,42 @@ export default function Header({ title, navigation }) {
     }
 
     return (
-        <View style={styles.header}>
-            <Ionicons name="ios-menu" size={30} onPress={openMenu} style={styles.icon} />
-            <View>
-                <Text style={styles.headerText}>{title}</Text>
+        <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
+            <View style={styles.header}>
+                <Ionicons name="ios-menu" size={30} onPress={openMenu} style={styles.icon} />
+                <View style={styles.headerTitle}>
+                    <Image source={require('../assets/heart_logo.png')} style={styles.headerImage} />
+                    <Text style={styles.headerText}>{title}</Text>
+                </View>
             </View>
-        </View>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     header: {
-
         width: '100%',
         height: '100%',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'grey'
     },
     headerText: {
         fontWeight: 'bold',
         fontSize: 20,
-        color: 'yellow',
+        color: '#333',
         letterSpacing: 1,
     },
     icon: {
         position: 'absolute',
         left: 16,
-    }
+    },
+    headerTitle: {
+        flexDirection: 'row'
+    },
+    headerImage: {
+        width: 30,
+        height: 30,
+        marginHorizontal: 10
+    },
 });
